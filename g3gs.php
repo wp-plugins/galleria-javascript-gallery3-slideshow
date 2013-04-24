f5d0a2f21a8c1766662016778b1ed57c05d03914
BilliardGreg <BilliardGreg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-24 21:07:28 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/galleria-javascript-gallery3-slideshow/trunk@703078 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ g3gs.php
@@ -0,0 +1,180 @@
+<?php
+/*
+Plugin Name: Galleria Javascript Slideshow fed from Menalto Gallery3 Album
+
+Plugin URI: http://www.gregwhitehead.us/
+
+Description: Galleria Javascript Slideshow (http://galleria.io/) that is fed from an external Menalto Gallery3 (http://galleryproject.org/) album rss feed.  Creates a one page javascript slideshow with autoplay with time being able to be set via slidespeed or set to false for no autoplay.  Other settings able to be set through shortcode call. Includes all galleria javascript files needed.  [g3gs albumnum="81" slidespeed="3000" preload="2" showinfo="false" imagecrop="false" transition="fade" clicknext="true" pauseoninteraction="true" gallerypath="/g3/"]
+
+Version: 1.0
+Author: Greg Whitehead
+Author URI: http://www.gregwhitehead.us/
+*/
+
+
+
+function gallery3galleriaslideshow_method() {
+
+	$pluginUrl = plugin_dir_url( __FILE__ );
+
+	wp_enqueue_script(
+
+		'custom-script',
+
+		$pluginUrl . '/galleria/galleria-1.2.8.min.js',
+
+		array( 'jquery' )
+
+	);
+
+}
+
+
+
+add_action( 'wp_enqueue_scripts', 'gallery3galleriaslideshow_method' );
+
+
+
+
+
+function gallery3galleriaslideshow($atts) {
+
+	$pluginUrl = plugin_dir_url( __FILE__ );
+
+
+
+
+	extract(shortcode_atts(array(
+
+		'albumnum' => '1',
+
+		'slidespeed' => '2000',
+
+		'preload' => '2',
+		'showinfo' => 'false',
+		'imagecrop' => 'false',
+		'transition' => 'fade',
+		'clicknext' => 'true',
+		'pauseoninteraction' => 'true',
+		'gallerypath' => '/gallery/',
+
+
+	), $atts));
+
+		
+
+	$newHtml = '<style>
+
+	#galleria{
+
+				position: relative;
+
+				height:600px;
+
+				margin-top:30px;
+
+				background-color: #C9E0ED; /*rgb(0,0,0); */
+
+				-webkit-box-shadow: 0px 0px 12px 3px rgba(153, 153, 153, .4);
+
+				-moz-box-shadow: 0px 0px 12px 3px rgba(153, 153, 153, .4);
+
+				box-shadow: 0px 0px 12px 3px rgba(153, 153, 153, .4);
+
+			}
+
+	 
+
+	</style>
+
+	
+
+		<div class="content">
+
+			<div id="galleria"><img style="position:absolute;top:20px;right:20px;" src="'.$pluginUrl.'/galleria/themes/classic/classic-loader.gif"/></div>
+
+			<div id="theButton" class="play"><a href="#"></a></div>
+
+		</div>
+
+		<script>
+
+			var gallery, images = [], tmpImage, tmpBigImage ;
+
+			jQuery(document).ready(function(){
+
+				jQuery(\'title\').text(\'Galleria Classic Theme\');
+
+				Galleria.loadTheme(\''.$pluginUrl.'/galleria/themes/classic/galleria.classic.min.js\');
+
+				getItems(\''.$gallerypath.'index.php/rss/feed/gallery/album/'. $albumnum.'\');
+
+			});
+
+			function getItems(earl){
+
+				jQuery.get(earl, {}, function(data){
+				var next = jQuery(data).find(\'atom\\\\:link[rel="next"]\').attr(\'href\');
+				//console.log(earl);
+				jQuery(data).find(\'item\').each(function(){
+					tmpImage = jQuery(this).children(\'media\\\\:group\').children(\'media\\\\:content\').first().attr(\'url\');
+					tmpBigImage = jQuery(this).children(\'media\\\\:group\').children(\'media\\\\:content\').last().attr(\'url\');
+					if (tmpImage == \'\' || tmpImage == null) tmpImage = jQuery(this).children(\'media\\\\:content\').attr(\'url\');
+					if (tmpBigImage == \'\' || tmpBigImage == null) tmpBig = jQuery(this).children(\'media\\\\:content\').attr(\'url\');
+					images.push({
+						image: tmpImage,
+						thumb: jQuery(this).children(\'media\\\\:thumbnail\').attr(\'url\'),
+						big: tmpBigImage ,
+						title: jQuery(this).children(\'title\').text(),
+						description: jQuery(this).children(\'description\').text(),
+						link: jQuery(this).children(\'link\').text()
+					});
+				});
+				if (next != undefined) {
+					getItems(next);
+				} else {
+					jQuery(\'#galleria\').galleria({
+						data_source: images,
+						showInfo: '.$showinfo.',
+						imageCrop: '.$imagecrop.',
+						transition: \''.$transition.'\',
+						clicknext:'.$clicknext.',
+				
+					autoplay:'.$slidespeed.',
+				
+					pauseOnInteraction:'.$pauseoninteraction.',
+				
+					preload: '.$preload.',
+				
+				
+					});
+					jQuery(\'#theButton, #pure\').show(\'slow\');
+					gallery = Galleria.get(0);
+					jQuery(\'.play a\').hover(function(){jQuery(this).addClass(\'over\');},function(){jQuery(this).removeClass(\'over\');});
+					jQuery(\'.play a\').on(\'click\', function(e){
+						e.preventDefault();
+						jQuery(\'#pure\').remove();
+						jQuery(this).parent().toggleClass(\'pause\').toggleClass(\'play\');
+						gallery.playToggle();
+					});
+				}
+
+				});
+
+				return
+
+			}
+
+		</script>';
+
+		
+
+		return $newHtml; 
+
+}
+
+
+
+add_shortcode ('g3gs','gallery3galleriaslideshow');
+
+	?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Galleria Javascript Gallery3 Slideshow ===
+Contributors: billiardgreg
+Donate link: http://www.billiardgreg.com/sponsors-and-links/
+Tags: slideshow, gallery, images, image, javascript, jquery, api, integration, gallery3, menalto, galleria
+Requires at least: 3.5
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The Galleria Javascript Slideshow fed from Menalto Gallery3 Album.
+
+== Description ==
+
+Galleria Javascript Slideshow (http://galleria.io/) that is fed from an external Menalto Gallery3 (http://galleryproject.org/) album rss feed.  Creates a one page javascript slideshow with autoplay with time being able to be set via slidespeed or set to false for no autoplay.  Other settings able to be set through shortcode call.  Includes all galleria javascript files needed.  [g3gs albumnum="81" slidespeed="3000" preload="2" showinfo="false" imagecrop="false" transition="fade" clicknext="true" pauseoninteraction="true" backgroundcolor="#fff" gallerypath="/g3/"]
+
+== Installation ==
+
+This section describes how to install the plugin.
+
+e.g.
+
+1. Upload the directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Where can I get answers to questions? =
+
+You can email greg@billiardgreg.com to receive answers or go to http://www.gregwhitehead.us/wordpress-plugins/galleria-javascript-gallery3-slideshow/
+
+
+== Screenshots ==
+
+1. No Screenshots
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release of Plugin
+
+== Upgrade Notice ==
+
+= 1.0 =
+* Initial Release of Plugin
